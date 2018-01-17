bd9de10c51b145ba42a3b7e97b530a1e95335f9e
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-17 02:21:42 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,20 @@
 // declare your functions here...
 function paragraphSelector() {
-  $('p');
+  return $('p');
 }
 
 function lastImageSelector() {
-  $('img:last')
+  return $('img:last')
 }
 
 function ninjaBabySelector() {
-  $('img')
+  return $('img')
 }
 
 function divSelector() {
-  $('div.pics')
+  return $('div.pics')
 }
 
 function firstListItem() {
-  $('ul li:first-child#pic-list')
+  return $('ul li:first-child#pic-list')
 }
